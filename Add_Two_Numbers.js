2278ed96cc44291c6c888d2b5499769dda38fadd
ecekpo <ememekpo@yahoo.com>
2023-05-12 07:31:05 +0100

    create listNode with value of zero and assign it to sum

--- Add_Two_Numbers.js
+++ Add_Two_Numbers.js
@@ -1,5 +1,17 @@
 //You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 // Define a function to add two linked lists
 var addTwoNumbers = function(l1, l2) {
+//Initiaalize the carry variable to zero
+let carry = 0;
 
+//create a new ListNode with a value of zero and assign it to sum
+let sum = new ListNode(0);
+
+// Create a currentNode variable and assign it to the sum
+let currentNode = sum;
+
+//Execute while loop 
+
+
+//Return the next node of sum, which is the actual sum of l1 and l2
 }
