130cd3dbbd487bd834c65ad5ccec128fc78e9272
ecekpo <ememekpo@yahoo.com>
2023-05-12 08:14:25 +0100

    solution for add_two_numbers

--- Add_Two_Numbers.js
+++ Add_Two_Numbers.js
@@ -1,26 +1,45 @@
 //You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-// Define a function to add two linked lists
-var addTwoNumbers = function(l1, l2) {
-//Initiaalize the carry variable to zero
-let carry = 0;
-
-//create a new ListNode with a value of zero and assign it to sum
-let sum = new ListNode(0);
-
-// Create a currentNode variable and assign it to the sum
-let currentNode = sum;
-
-//Execute while loop 
-// If l1 exists, assign its value to val1; otherwise, assign zero
-let val1 = l1 ? l1.val : 0;
-// If l2 exists, assign its value to val2; otherwise, assign zero
-let val2 = l2 ? l2.val : 0;
-// Add val1, val2, and carry, and assign the result to currentSum
-let currentSum = val1 + val2 + carry;
-        // If currentSum is greater than or equal to 10, set carry to 1; otherwise, set it to 0
-        carry = currentSum >= 10 ? 1 : 0;
-}
-
-
-//Return the next node of sum, which is the actual sum of l1 and l2
-}
+class ListNode {
+    constructor(val = 0, next = null) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+  
+  function addTwoNumbers(l1, l2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+  
+    while (l1 !== null || l2 !== null) {
+      let l1Val = l1 !== null ? l1.val : 0;
+      let l2Val = l2 !== null ? l2.val : 0;
+      let sum = l1Val + l2Val + carry;
+      carry = Math.floor(sum / 10);
+  
+      current.next = new ListNode(sum % 10);
+      current = current.next;
+  
+      let l1Next = l1 !== null ? l1.next : null;
+      let l2Next = l2 !== null ? l2.next : null;
+      l1 = l1Next;
+      l2 = l2Next;
+    }
+  
+    if (carry > 0) {
+      current.next = new ListNode(carry);
+    }
+  
+    return dummy.next;
+  }
+  
+  let l1 = new ListNode(2);
+  l1.next = new ListNode(4);
+  l1.next.next = new ListNode(3);
+  
+  let l2 = new ListNode(5);
+  l2.next = new ListNode(6);
+  l2.next.next = new ListNode(4);
+  
+  console.log(addTwoNumbers(l1, l2));
+  
