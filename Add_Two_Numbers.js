5e0cb5ed75efed810c54a0c853c70e2e526e062f
ecekpo <ememekpo@yahoo.com>
2023-05-12 07:26:30 +0100

    define two functions to add two linked lists

---
+++ Add_Two_Numbers.js
@@ -0,0 +1,5 @@
+//You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// Define a function to add two linked lists
+var addTwoNumbers = function(l1, l2) {
+
+}
